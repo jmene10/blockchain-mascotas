99f8adc27eeceebe26c837eaca94867e78a83d3c
Juanjo Meneses <juanjomeneses10@gmail.com>
2023-04-12 10:22:58 +0200

    algunos cambios

---
+++ src/formulario.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    App.init();
+});
+
+
+const menuForm = document.querySelector("#menuForm");
+//Obtiene los datos del formulario
+menuForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const primerPlato = menuForm["primerPlato"].value;
+    const segundoPlato = menuForm["segundoPlato"].value;
+    const disponible = menuForm["disponible"].value;
+    const precio = menuForm["precio"].value;
+
+    App.createHouse(primerPlato, segundoPlato, disponible, precio);
+});
+
+const menuList = document.querySelector("#menuList");
+//Obtiene el atributo id del boton de comprar que corresponde a cada casa
+menuList.addEventListener("click", (e) => {
+    e.preventDefault();
+    const menuId = e.target.getAttribute('id');
+
+    App.buyMenu(menuId);
+});
